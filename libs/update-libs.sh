e098f5100970497faaed27a07ffa8434dc1beb0f
Codifies <nospam@antispam.com>
2014-09-06 12:42:08 +0100

    script to update gdx libs after update/recompile

--- libs/update-libs.sh
+++ libs/update-libs.sh
@@ -1,5 +1,20 @@
-gdx-backend-lwjgl.jar
-gdx-backend-lwjgl-natives.jar
-gdx.jar
-gdx-natives.jar
-update-libs.sh
+#!/bin/bash
+
+rm -f *.jar
+
+gdxdist="../../libgdx/dist/"
+
+jars=(
+"gdx.jar"
+"gdx-natives.jar"
+"gdx-backend-lwjgl.jar"
+"gdx-backend-lwjgl-natives.jar"
+"extensions/gdx-box2d/gdx-box2d.jar"
+"extensions/gdx-box2d/gdx-box2d-natives.jar"
+)
+
+for jar in ${jars[@]}
+do
+    echo $jar
+    cp $gdxdist$jar .
+done
