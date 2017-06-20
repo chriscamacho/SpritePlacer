1329496ed90023fb76cea2cede8f23e55bd1434c
chris <nospam@antispam.com>
2017-06-20 23:32:37 +0100

    update for minor change in gdx

--- file-dialog/src/uk/co/bedroomcoders/fileDialog/fileDialog.java
+++ file-dialog/src/uk/co/bedroomcoders/fileDialog/fileDialog.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.Input.Keys;
 
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.utils.Align;
 
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
